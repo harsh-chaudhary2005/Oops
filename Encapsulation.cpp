539c0298a5c89bddb177276162ef1e04c9df1bd3
Harsh Chaudhary <hc492250@gmail.com>
2026-08-15 22:32:57 +0530

    Add files via upload

---
+++ Encapsulation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Account{
+private:
+    double balance;
+    string password;    //Data hiding
+public:
+    string name;
+    string accountID;
+    
+};
+int main(){
+  
+  return 0;
+}
---
+++ class.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher{
+private:
+    double salary;
+public:    //very very very important step (Access modifier)
+    //properties
+    string name;
+    string dept;
+    string subject;
+    
+
+    //methods(functions hi h bss class ke andr h we call it member function)
+    void changeDept(string newdept){
+        dept = newdept;
+    }
+    //setter function
+    void setSalary(double s){
+        salary = s;
+    }
+    //getter function
+    double getSalary(){
+        return salary;
+    }
+};
+int main(){
+  Teacher t1;
+  t1.name = "Pushpa";
+  t1.dept = "ECE";
+  t1.subject = "MTT";
+  t1.setSalary(25000);
+  
+
+  cout << t1.getSalary() << endl;
+  return 0;
+}
